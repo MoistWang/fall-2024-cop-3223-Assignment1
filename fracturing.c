40e210d6532e77a27c8c36eb84c7586626a3fecb
MoistWang <55607089+MoistWang@users.noreply.github.com>
2024-09-05 23:10:04 +0000

    Added source file.

---
+++ fracturing.c
@@ -0,0 +1,55 @@
+//********************************************************
+//
+// fracturing.c
+// Author: Michael Wang (UCFID 5611386)
+// Class: COP 3223, Professor Parra
+// Submission for Assingment 1 
+//
+//********************************************************
+
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159
+
+double calculateDistance();
+double calculatePerimeter();
+double calculateArea();
+double calculateWidth();
+double calculateHeight();
+double askForUserInput();
+
+int main(int argc, char **argv)
+{
+    calculateDistance();
+}
+
+double calculateDistance()
+{
+    double distance = sqrt(pow(x2-x1)+pow(y2-y1));
+    printf("Point #1 entered: x1 = %d; y1 = %d\n", x1, y1);
+    printf("Point #2 entered: x1 = %d; y1 = %d", x2, y2);
+    printf("The distance between the two points is %f", distance);
+}
+
+double calculatePerimeter()
+{
+
+}
+double calculateArea()
+{
+
+}
+double calculateWidth()
+{
+
+}
+double calculateHeight()
+{
+
+}
+double askForUserInput()
+{
+
+}
+
